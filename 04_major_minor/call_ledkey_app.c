a7436a6c0283aa0084fea6ef8eb35f6623df5408
c_lan <ffx120201@gmail.com>
2026-08-19 14:38:37 +0900

    Update call_ledkey_app.c

--- 04_major_minor/call_ledkey_app.c
+++ 04_major_minor/call_ledkey_app.c
@@ -1,1 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+#define WRITE_DEVICE_FILENAME "/dev/minor_led"
+#define READ_DEVICE_FILENAME  "/dev/minor_key"
+#define DEVICE_FILENAME       "/dev/minor_ledkey"
+
+int main()
+{
+	int read_dev;
+	int write_dev;
+	int readwrite_dev;
+
+	char buff[128];
+	int loop;
+
+	read_dev = open(READ_DEVICE_FILENAME, O_RDONLY | O_NDELAY);
+	if(read_dev < 0)
+	{
+		perror("open()");
+		printf(READ_DEVICE_FILENAME " open error\n");
+		exit(1);
+	}
+
+	write_dev = open(WRITE_DEVICE_FILENAME, O_WRONLY | O_NDELAY);
+	if(write_dev < 0)
+	{
+		perror("open()");
+		printf(WRITE_DEVICE_FILENAME " open error\n");
+
+		close(read_dev);
+		exit(1);
+	}
+
+	readwrite_dev = open(DEVICE_FILENAME, O_RDWR | O_NDELAY);
+	if(readwrite_dev < 0)
+	{
+		perror("open()");
+		printf(DEVICE_FILENAME " open error\n");
+
+		close(read_dev);
+		close(write_dev);
+		exit(1);
+	}
+
+	printf("wait... input\n");
+
+	while(1)
+	{
+		if(read(read_dev, buff, 1) == 1 && buff[0] != 0)
+		{
+			printf("read data [%#04X]\n", buff[0] & 0xFF);
+
+			if(buff[0] & 0x80)
+				break;
+		}
+	}
+
+	printf("input ok...\n");
+
+	printf("led flashing...\n");
+
+	for(loop = 0; loop < 5; loop++)
+	{
+		buff[0] = 0xFF;
+		write(write_dev, buff, 1);
+		sleep(1);
+
+		buff[0] = 0x00;
+		write(write_dev, buff, 1);
+		sleep(1);
+	}
+
+	printf("wait... input\n");
+
+	while(1)
+	{
+		if(read(readwrite_dev, buff, 1) == 1 && buff[0] != 0)
+		{
+			printf("read data [%#04X]\n", buff[0] & 0xFF);
+
+			write(readwrite_dev, buff, 1);
+
+			if(buff[0] & 0x80)
+				break;
+		}
+	}
+
+	close(read_dev);
+	close(write_dev);
+	close(readwrite_dev);
+
+	return 0;
+}
